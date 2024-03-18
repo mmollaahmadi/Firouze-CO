b24aa8466f9d614c643f1a18831a882bcc35ee7a
Mohammad Mollaahmadi <mollaahmadimohammad@gmail.com>
2024-03-18 18:00:05 +0330

    add script

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -5,6 +5,7 @@ import { useRouter } from "next/router";
 import { useContext } from "react";
 import { GlobalContext } from "@/context";
 import Footer from "@/components/composite/Footer";
+import Script from "next/script";
 
 export default function Home() {
   const router = useRouter();
@@ -14,13 +15,20 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Alibab Countries Challenge</title>
+        <title>Firouze Faam AR Products</title>
         <meta
           name="description"
           content="Firouze Faam Products"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
+        {/* <Script
+          async
+          strategy="afterInteractive"
+          type="module"
+          src="https://unpkg.com/@google/model-viewer@^2.1.1/dist/model-viewer.min.js"
+        /> */}
+        {/* <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.4.0/model-viewer.min.js"></script> */}
       </Head>
       <main
         className={`min-h-[100vh] flex flex-col justify-between ${
