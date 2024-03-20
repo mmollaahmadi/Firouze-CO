59847781b77d51b1a3cfa0340b1def8da700903d
Mohammad Mollaahmadi <mollaahmadimohammad@gmail.com>
2024-03-20 11:15:35 +0330

    move the buton to outside of model viewer.

--- src/components/screen/Detail/Detail.jsx
+++ src/components/screen/Detail/Detail.jsx
@@ -1,21 +1,14 @@
-import Image from "next/image";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ProductInformationSection from "@/components/composite/ProductInformationSection";
 import { useRouter } from "next/router";
-import { GlobalContext } from "@/context";
 import ToolbarSection from "./Toolbar.section";
 import useProducts from "@/logic/client/useProducts";
-// import dynamic from 'next/dynamic';
-
-// const ModelViewer = require('@google/model-viewer/dist/model-viewer.min.js') as any;
-// import "@google/model-viewer";
 
 export default function Detail() {
   const router = useRouter();
   const { id } = router.query;
-  // const { products } = useContext(GlobalContext);
-  const { errorMessage, loading, products } = useProducts();
-  console.log("id ", id, products, loading);
+  const { loading, products } = useProducts();
+  const innerButtonRef = useRef(null); // Ref for inner button
 
   const findCountryData = (id) => {
     try {
@@ -40,7 +33,7 @@ export default function Detail() {
     <div
       className={"flex flex-col mb-auto p-6 sm:py-4 sm:px-14 bg-transparent"}
     >
-      <ToolbarSection />
+      <ToolbarSection innerButtonRef={innerButtonRef} />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-6 md:grid-rows-1">
         {/* <div className="col-span-1 col-row-1 my-auto">
           <Image
@@ -53,37 +46,13 @@ export default function Detail() {
           />
         </div> */}
         <div className="col-span-1 col-row-1 my-auto">
-          {/* <model-viewer
-            // id={`modelViewer${product?.id}`}
-            alt="AR Model"
-            ar
-            ar-modes="webxr scene-viewer quick-look"
-            src={product?.ar_model ?? ""}
-            camera-controls
-            ar-scale="fixed"
-            ar-placement="wall"
-            camera-orbit="auto auto auto"
-            min-camera-orbit="auto 75deg auto"
-            max-camera-orbit="auto 75deg auto"
-            // environment-image="/model/moon_1k.hdr"
-            style={{ width: "100%", height: "500px" }}
-          >
-            <button
-              slot="ar-button"
-              id="ar-button"
-              className="bg-takeda-red font-normal tracking-wide text-white"
-            >
-              View in your space
-            </button>
-          </model-viewer> */}
-          {product && (
+          {!loading && (
             <model-viewer
               alt="AR Model"
               src={product?.ar_model ?? ""}
               ar
-              environment-image="/model/moon_1k.hdr"
+              environment-image="null"
               poster={product?.image ?? ""}
-              // poster="/model/NeilArmstrong.webp"
               shadow-intensity="1"
               camera-controls
               touch-action="pan-y"
@@ -91,17 +60,19 @@ export default function Detail() {
                 width: "100%",
                 height: "500px",
                 border: "1px dashed gray",
-
                 borderRadius: "15px",
               }}
             >
-            <button
-            slot="ar-button"
-            id="ar-button"
-            className="bg-takeda-red font-normal tracking-wide text-black"
-          >
-            View in your space
-          </button>
+              <button
+                slot="ar-button"
+                id="ar-button"
+                ref={innerButtonRef}
+                style={{
+                  display: "none",
+                }}
+              >
+                Display on the wall
+              </button>
             </model-viewer>
           )}
         </div>
--- src/components/screen/Detail/Toolbar.section.tsx
+++ src/components/screen/Detail/Toolbar.section.tsx
@@ -2,7 +2,11 @@ import Button from "@/components/atomic/Button";
 import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
-export default function ToolbarSection() {
+export default function ToolbarSection({
+  innerButtonRef,
+}: {
+  innerButtonRef: any;
+}) {
   const router = useRouter();
   return (
     <div className="flex flex-row w-full justify-start items-center my-6">
@@ -16,6 +20,12 @@ export default function ToolbarSection() {
         onClick={() => router.push("/")}
         label={"Home"}
         className="mx-2"
+      />
+      <Button
+        label={"Display on the wall"}
+        onClick={() => {
+          innerButtonRef.current.click();
+        }}
       />
     </div>
   );
