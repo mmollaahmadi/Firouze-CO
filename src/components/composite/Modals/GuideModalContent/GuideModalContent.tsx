5898d29ee790b287e76a1601b8ff6a0093dfa19d
Mohammad Mollaahmadi <mollaahmadimohammad@gmail.com>
2024-04-19 00:20:32 +0330

    change color of modal.

--- src/components/composite/Modals/GuideModalContent/GuideModalContent.tsx
+++ src/components/composite/Modals/GuideModalContent/GuideModalContent.tsx
@@ -31,26 +31,33 @@ export default function GuideModalContent({
   };
 
   return (
-    <div className="grid grid-cols-1 p-6 justify-evenly items-center shadow-md bg-opacity-95 bg-SuperDarkBlue rounded-lg absolute w-5/6 sm:w-2/5 top-0 mt-[30vh] left-1/2 -translate-x-1/2 bottom-70">
-      <div className="text-center">
-        <h1 className="text-lg font-bold">{context?.guide_lines_title}</h1>
-      </div>
-      <div className="px-2 py-8">
-        {context?.guide_lines?.map((a) => (
-          <li>{a}</li>
-        ))}
-      </div>
-      <label className="flex items-center">
-        <input
-          type="checkbox"
-          checked={isChecked}
-          onChange={handleCheckboxChange}
-          className="mx-2"
-        />
-        {context?.never_show_me_again}
-      </label>
-      <div className="flex justify-center mt-2">        
-        <Button onClick={() => displayModel()} label={context?.ok} />
+    <div className="fixed inset-0 flex w-full h-full justify-center items-center bg-[#000] bg-opacity-40 z-50">
+      <div className="grid grid-cols-1 p-6 justify-evenly items-center shadow-md bg-White text-SuperDarkBlue rounded-lg absolute w-5/6 sm:w-2/5 top-0 mt-[30vh] left-1/2 -translate-x-1/2 bottom-70">
+        <div className="text-center">
+          <h1 className="text-lg font-bold">{context?.guide_lines_title}</h1>
+        </div>
+        <div className="px-2 py-8">
+          {context?.guide_lines?.map((a) => (
+            <li>{a}</li>
+          ))}
+        </div>
+        <label className="flex items-center">
+          <input
+            type="checkbox"
+            checked={isChecked}
+            onChange={handleCheckboxChange}
+            className="mx-2"
+          />
+          {context?.never_show_me_again}
+        </label>
+        <div className="flex justify-center mt-2">
+          <button
+            className={`bg-DarkBlue text-White w-fit max-w-56 text-sm px-4 py-1 mx-2 h-fit shadow-md rounded-sm flex flex-row justify-around items-center`}
+            onClick={() => displayModel()}
+          >
+            {context?.ok}
+          </button>
+        </div>
       </div>
     </div>
   );
