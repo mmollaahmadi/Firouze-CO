72bfe5bacfc64fb3d142131f0f5fe990acc463dc
Mohammad Mollaahmadi <mollaahmadimohammad@gmail.com>
2024-04-18 23:36:56 +0330

    change width of modal and message.

--- src/components/composite/Modals/GuideModalContent/GuideModalContent.tsx
+++ src/components/composite/Modals/GuideModalContent/GuideModalContent.tsx
@@ -1,7 +1,7 @@
 import Button from "@/components/atomic/Button";
 import { GlobalContext } from "@/context";
 import useLanguage from "@/logic/client/useLanguage";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 
 export default function GuideModalContent({
   onClose,
@@ -13,10 +13,14 @@ export default function GuideModalContent({
   const { language } = useContext(GlobalContext);
   const { getContext } = useLanguage();
   const context = getContext(language);
+  const [isChecked, setIsChecked] = useState(false);
+  const handleCheckboxChange = (event: any) => {
+    setIsChecked(event.target.checked);
+  };
 
-  const displayModel = (showMeAgain: boolean = true) => {
+  const displayModel = () => {
     try {
-      if (!showMeAgain) {
+      if (isChecked === true) {
         sessionStorage?.setItem("displayModal", "false");
       }
       innerButtonRef?.current?.click();
@@ -25,21 +29,27 @@ export default function GuideModalContent({
       console.error(error);
     }
   };
+
   return (
-    <div className="grid grid-cols-1 p-6 justify-evenly items-center shadow-md bg-opacity-95 bg-SuperDarkBlue rounded-lg absolute w-250 top-0 mt-[30vh] left-1/2 -translate-x-1/2 bottom-70">
+    <div className="grid grid-cols-1 p-6 justify-evenly items-center shadow-md bg-opacity-95 bg-SuperDarkBlue rounded-lg absolute w-5/6 sm:w-2/5 top-0 mt-[30vh] left-1/2 -translate-x-1/2 bottom-70">
       <div className="text-center">
         <h1 className="text-lg font-bold">{context?.guide_lines_title}</h1>
       </div>
-      <div className="p-8">
+      <div className="px-2 py-8">
         {context?.guide_lines?.map((a) => (
           <li>{a}</li>
         ))}
       </div>
-      <div className="flex justify-center">
-        <Button
-          onClick={() => displayModel(false)}
-          label={context?.ok_and_never_show_me_again}
+      <label className="flex items-center">
+        <input
+          type="checkbox"
+          checked={isChecked}
+          onChange={handleCheckboxChange}
+          className="mx-2"
         />
+        {context?.never_show_me_again}
+      </label>
+      <div className="flex justify-center mt-2">        
         <Button onClick={() => displayModel()} label={context?.ok} />
       </div>
     </div>
